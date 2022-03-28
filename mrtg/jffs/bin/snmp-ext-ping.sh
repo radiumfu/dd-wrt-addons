657de959e18d04495e5568f8c8b293c33d5e9496
blue-sky-r <robert.puskajler@yahoo.ca>
2022-03-28 20:01:31 +0200

    snmpd extension script - avg ping time

---
+++ mrtg/jffs/bin/snmp-ext-ping.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SNMP extension for DD-WRT - average ping time to $1 from $2 times [ms] with $3 timeout #
+#
+target=${1:-localhost}
+ping -c ${2:-1} -w ${3:-5} $target | awk -F/ '/min\/avg\/max/ {print $4}'
