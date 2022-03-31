c7d2b0df92f9457ba3ce38b7483f82446e58a2e5
blue-sky-r <robert.puskajler@yahoo.ca>
2022-03-31 22:53:40 +0200

    snmpd extension script - wl noise - return 0 if radio is off

--- mrtg/jffs/bin/snmp-ext-wifi-noise-dbm.sh
+++ mrtg/jffs/bin/snmp-ext-wifi-noise-dbm.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # SNMP extension for DD-WRT - wireless noise floor [dBm] #
+# radio is off
+[ $(wl radio) = "0x0001" ] && exit 0
+# radio is on - get noise (even if radio is off the noise is returned)
 # $1 is optional offset to get positive numbers for mrtg (default 100)
 let n=${1:-100}+$(wl noise)
 echo $n
