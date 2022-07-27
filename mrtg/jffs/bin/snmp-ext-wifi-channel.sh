efd901983e9f553adb284c9cf99b5fae111801fb
blue-sky-r <robert.puskajler@yahoo.ca>
2022-07-27 23:27:27 +0200

    snmpd extension script - wifi channel - returns 0 if radio is OFF

--- mrtg/jffs/bin/snmp-ext-wifi-channel.sh
+++ mrtg/jffs/bin/snmp-ext-wifi-channel.sh
@@ -2,6 +2,6 @@
 
 # SNMP extension for DD-WRT - wireless primary channel #
 # radio is off
-[ $(wl radio) = "0x0001" ] && exit 0
+[ $(wl radio) = "0x0001" ] && echo "0" && exit
 # radio is on - get primary channel (even if rdio is off primary channel is displayed)
 wl assoc | awk '/Primary channel:/ {print $3}'
