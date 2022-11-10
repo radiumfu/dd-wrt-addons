3f87aa1bbae502137fe3e5c4709d924f21f42af7
blue-sky-r <robert.puskajler@yahoo.ca>
2022-11-10 09:46:10 +0100

    refactored to be more parametric

--- mrtg/jffs/bin/snmp-ext-he-con-cnt.sh
+++ mrtg/jffs/bin/snmp-ext-he-con-cnt.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# SNMP extension for DD-WRT - count of He vlan3 connections #
-#
-vlan2='192.168.1.'
-vlan3='192.168.3.'
-gw2=${vlan2}11
-gw3=${vlan3}11
+# SNMP extension for DD-WRT - count vlan connections to wan #
+# usage: $0 <vlan-gateway> <local-gateway> [<filter>]
 
-grep $vlan3 /proc/net/nf_conntrack | grep -v $gw3 | grep -v $vlan2 | grep -c "$1"
+gw3=${1:-192.168.3.1}
+gw1=${2:-192.168.1.1}
+match=$3
+# connection tracking
+con=/proc/net/nf_conntrack
+# extract subnet from gateway
+vlan3=${gw3%.*}.
+vlan1=${gw1%.*}.
+# get vlan conn  - vlan.gw conn - local.lan conn - filter.count
+grep $vlan3 $con | grep -v $gw3 | grep -v $vlan1 | grep -c "$match"
