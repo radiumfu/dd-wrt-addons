4d7adffeb2d459f1f98f3fbf2f4255bfa6a224fa
blue-sky-r <robert.puskajler@yahoo.ca>
2024-02-19 18:55:28 +0100

    updated for r55052 (wl radio)

--- mrtg/jffs/bin/snmp-ext-wifi-channel.sh
+++ mrtg/jffs/bin/snmp-ext-wifi-channel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # SNMP extension for DD-WRT - wireless primary channel #
-# radio is off
-[ $(wl radio) = "0x0001" ] && echo "0" && exit
+# if radio is not on
+[ $(wl radio) != 0x0000 ] && echo "0" && exit
 # radio is on - get primary channel (even if rdio is off primary channel is displayed)
 wl assoc | awk '/Primary channel:/ {print $3}'
--- mrtg/jffs/bin/snmp-ext-wifi-noise-dbm.sh
+++ mrtg/jffs/bin/snmp-ext-wifi-noise-dbm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # SNMP extension for DD-WRT - wireless noise floor [dBm] #
-# radio is off
-[ $(wl radio) = "0x0001" ] && echo "0" && exit
+# if radio is not on
+[ $(wl radio) != 0x0000 ] && echo "0" && exit
 # radio is on - get noise (even if radio is off the noise is returned)
 # $1 is optional offset to get positive numbers for mrtg (default 100)
 let n=${1:-100}+$(wl noise)
